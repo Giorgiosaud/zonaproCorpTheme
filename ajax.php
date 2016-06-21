8a1ab1c8c5709082312ec65dc77c8e078191bb1b
Jorge Saud <jorgelsaud@gmail.com>
2016-06-20 23:01:24 -0400

    fixes

--- ajax.php
+++ ajax.php
@@ -69,7 +69,7 @@ if(!function_exists('sendEmail')){
 	//$headers .= "CC: jorgelsaud@gmail.com\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-	mail($emailTo,$subject,$mensajeEmail,$headers);
+	wp_mail($emailTo,$subject,$mensajeEmail,$headers);
 	die();
 
 	}
