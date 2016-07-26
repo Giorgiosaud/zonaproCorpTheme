33995ab93b21398ea121b1ffe9cfbce1897ec620
Jorge Saud <jorgelsaud@gmail.com>
2016-07-26 14:36:48 -0400

    fix gtag

--- header.php
+++ header.php
@@ -28,13 +28,13 @@ ga('send', 'pageview');
 <body <?php body_class(); ?>>
 
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WH884Q"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=<?php the_field('google_tsgs_seguimiento','options')?>"
 		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','<?php the_field('google_tags_seguimiento','options')?>');</script>
+})(window,document,'script','dataLayer','<?php the_field('google_tsgs_seguimiento','options')?>');</script>
 <!-- End Google Tag Manager -->
 <?php 
 get_template_part('menu','principal');
