3e1f9360f36f7c17c440bac6ac5b531bd745929f
Jorge Saud <jorgelsaud@gmail.com>
2016-09-04 15:11:44 -0400

    fix carusel

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -19,19 +19,15 @@ jQuery(document).ready(function($){
 		var p = new Parallax('.ImagenAnchoCompleto__imagen').init();
 	}
 	$('.PostCarusel').slick({
-		dots: true,
 		infinite: true,
-		speed: 300,
-		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToShow: 3,
+		slidesToScroll: 3,
 		responsive: [
 			{
 				breakpoint: 1024,
 				settings: {
 					slidesToShow: 3,
 					slidesToScroll: 3,
-					infinite: true,
-					dots: true
 				}
 			},
 			{
@@ -48,9 +44,6 @@ jQuery(document).ready(function($){
 					slidesToScroll: 1
 				}
 			}
-			// You can unslick at a given breakpoint now by adding:
-			// settings: "unslick"
-			// instead of a settings object
 		]
 	});
 });
