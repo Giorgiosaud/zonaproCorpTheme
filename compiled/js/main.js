e2e31ef3fee95613bf2a251a827fa5bb311d4032
Jorge Saud <jorgelsaud@gmail.com>
2016-09-04 15:46:42 -0400

    fix carusel

--- compiled/js/main.js
+++ compiled/js/main.js
@@ -19,32 +19,28 @@ jQuery(document).ready(function($){
 		var p = new Parallax('.ImagenAnchoCompleto__imagen').init();
 	}
 	$('.PostCarusel').slick({
-	infinite: true,
-	adaptiveHeight:true,
-	accessibility:true,
-	arrows:true,
-	slidesToShow: 3,
-	slidesToScroll: 3,
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		adaptiveHeight: true,
 	responsive: [
 		{
 			breakpoint: 1024,
 			settings: {
 				slidesToShow: 3,
-				slidesToScroll: 3,
 			}
 		},
 		{
 			breakpoint: 600,
 			settings: {
 				slidesToShow: 2,
-				slidesToScroll: 2
 			}
 		},
 		{
 			breakpoint: 480,
 			settings: {
 				slidesToShow: 1,
-				slidesToScroll: 1
 			}
 		}
 	]
